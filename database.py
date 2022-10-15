a737aac74780ddd3df6cde5fb30030afcdbfa448
Kai Kitagawa-Jones <kaikitagawajones@gmail.com>
2022-10-16 00:32:44 +0200

    delivery working

--- database.py
+++ database.py
@@ -14,7 +14,6 @@ import pymysql as sql
 
 verbose = False  # if this is true PizzaDatabase will print out every sql command before executing it, useful for debugging
 
-
 class PizzaDatabase:
     # constructor, connects to database "pizza" if it exists; if not it will create and initialise it
     def __init__(self):
@@ -25,26 +24,10 @@ class PizzaDatabase:
             self.__create_sql_database()
             self.__populate_sql_database()
 
-        self.execute("USE pizza;")
+        self.__execute("USE pizza;")
 
     # deconstructor, probably unnecessary, but who knows, might be good to have
     def __del__(self): self.db.close()
-
-    # executes sql command
-    # replaces newlines and tabs with spaces
-    # can run many commands at once, if they are separated with a ";"
-    # if "verbose" is set to true it will print the command before running it, pretty useful for debugging
-    def execute(self, command):
-        for line in command.replace("\n", " ").replace("\t", " ").split(";"):
-            if (len(line) == 0):
-                continue
-            try:
-                if verbose:
-                    print("RUNNING: " + line)
-                self.cursor.execute(line)
-                self.db.commit()
-            except sql.Error as error:
-                print("ERROR: " + str(error))
 
     # resets the sql database to its the state specified in the .sql files
     def reset(self):
@@ -53,174 +36,163 @@ class PizzaDatabase:
         self.__populate_sql_database()
 
     def get_pizza(self, id):
-        self.execute(
-            f"SELECT pizza.name, ingredient.id FROM ingredient JOIN pizza_to_ingredient ON ingredient.id = pizza_to_ingredient.ingredient JOIN pizza ON pizza.id = pizza_to_ingredient.pizza WHERE pizza.id = '{id}';")
+        self.__execute(f"SELECT pizza.name, ingredient.id FROM ingredient JOIN pizza_to_ingredient ON ingredient.id = pizza_to_ingredient.ingredient JOIN pizza ON pizza.id = pizza_to_ingredient.pizza WHERE pizza.id = '{id}';")
         info = self.cursor.fetchall()
         return {"name": info[0][0], "ingredients": [p[1] for p in info]}
 
     def get_ingredient(self, id):
-        self.execute(
-            "SELECT name, category, price FROM ingredient WHERE id = '" + str(id) + "';")
+        self.__execute("SELECT name, category, price FROM ingredient WHERE id = '" + str(id) + "';")
         ingredient = self.cursor.fetchone()
         return {"name": ingredient[0], "category": ingredient[1], "price": ingredient[2]}
 
     def get_side_dish(self, id):
-        self.execute(f"SELECT name, price FROM side_dish WHERE id= {id};")
+        self.__execute(f"SELECT name, price FROM side_dish WHERE id= {id};")
         side_dish = self.cursor.fetchone()
         return {"name": side_dish[0], "price": side_dish[1]}
 
-    def get_customer(self, cus_id):
-        if (not self.is_exist(table='customer', col='id', str=cus_id)): return None
-        else:
-            self.execute(f"SELECT name, address, postcode, phone_number FROM customer WHERE id = '{cus_id}';")
-            customer = self.cursor.fetchone()		
-            return {"name": customer[0], "address": customer[1], "postcode": customer[2], "phone_number": customer[3]}
+    def get_customer(self, id):
+        if (not self.exits(table='customer', col='id', str=id)): return None
+        self.__execute(f"SELECT name, address, postcode, phone_number FROM customer WHERE id = '{id}';")
+        customer = self.cursor.fetchone()		
+        return {"name": customer[0], "address": customer[1], "postcode": customer[2], "phone_number": customer[3]}
+
+    def get_order(self, id):
+        if (not self.exits(table='order_info', col='id', str=id)): return None
+        self.__execute(f"SELECT customer, time FROM order_info WHERE id = '{id}';")
+        order = self.cursor.fetchone()		
+        return {"customer": order[0], "time": order[1]}
+    
+    def get_deliveryman(self, id):
+        if (not self.exits(table='deliveryman', col='id', str=id)): return None
+        self.__execute(f"SELECT name, postcode, time FROM deliveryman WHERE id = '{id}';")
+        deliveryman = self.cursor.fetchone()		
+        return {"name": deliveryman[0], "postcode": deliveryman[1], "time": deliveryman[2]}
 
     # returns a list of all pizza ids
     def get_all_pizza_ids(self):
-        self.execute("SELECT id FROM pizza;")
+        self.__execute("SELECT id FROM pizza;")
         return [i[0] for i in self.cursor.fetchall()]
 
     def get_all_side_dish_ids(self):
-        self.execute("SELECT id FROM side_dish;")
+        self.__execute("SELECT id FROM side_dish;")
+        return [i[0] for i in self.cursor.fetchall()]
+    
+    def get_all_order_ids(self):
+        self.__execute("SELECT id FROM order_info;")
+        return [i[0] for i in self.cursor.fetchall()]
+    
+    def get_all_deliverymen_ids(self):
+        self.__execute("SELECT id FROM deliveryman;")
         return [i[0] for i in self.cursor.fetchall()]
 
-    def get_total_price(self, items):
-        price = 0
-        for pizza_id in items[0]:
-            pizza = self.get_pizza_info(pizza_id)
-            for ingredient_name in pizza["ingredients"]:
-                ingredient = self.get_ingredient(ingredient_name)
-                price += ingredient["price"]*1.4*1.09
-        for side_id in items[1]:
-            price += self.get_sidedish(side_id)[2]
-        return price
-
     def get_order_time(self, order_id):
-        if (not self.is_exist('order_info', 'id', order_id)):
+        if (not self.exits('order_info', 'id', order_id)):
             print("Order does not exist.")
             return
-        self.execute(f"SELECT time FROM order_info WHERE id = {order_id};")
+        self.__execute(f"SELECT time FROM order_info WHERE id = {order_id};")
         return self.cursor.fetchone()[0]
 
     def get_coupon(self):
-        self.execute("INSERT INTO coupon() VALUES();")
-        self.execute("SELECT id FROM coupon WHERE status = 0 LIMIT 1;")
+        self.__execute("INSERT INTO coupon() VALUES();")
+        self.__execute("SELECT id FROM coupon WHERE status = 0 LIMIT 1;")
         return self.cursor.fetchone()[0]
 
     def is_vegan(self, pizza_name):
-        self.execute(f"SELECT ingredient.category FROM ingredient JOIN pizza_to_ingredient ON ingredient.id = pizza_to_ingredient.ingredient JOIN pizza ON pizza.id = pizza_to_ingredient.pizza WHERE pizza.name = '" + pizza_name + "';")
+        self.__execute(f"SELECT ingredient.category FROM ingredient JOIN pizza_to_ingredient ON ingredient.id = pizza_to_ingredient.ingredient JOIN pizza ON pizza.id = pizza_to_ingredient.pizza WHERE pizza.name = '" + pizza_name + "';")
         category = [c[0] for c in self.cursor.fetchall()]
         for each in category:
-            if each != 'VEGETARIAN':
-                return ""
+            if each != 'VEGETARIAN': return ""
         return '(VEGETARIAN)'
 
-    def is_exist(self, table, col, str):
-        self.execute(
-            f"SELECT {col} FROM {table} WHERE {col} = '{str}' LIMIT 1;")
+    def exits(self, table, col, str):
+        self.__execute(f"SELECT {col} FROM {table} WHERE {col} = '{str}' LIMIT 1;")
         return self.cursor.fetchone() != None
 
     def create_customer(self, customer_name, address, postcode, phoneNo):
-        try:
-            self.execute(
-                f"INSERT INTO customer(name, address, postcode, phone_number) values ('{customer_name}', '{address}', '{postcode}', '{phoneNo}');")
-            self.execute("SELECT last_insert_id();")
-            return self.cursor.fetchone()[0]
-        except sql.Error as error:
-            print("ERROR: " + str(error))
-            return False
+        self.__execute(f"INSERT INTO customer(name, address, postcode, phone_number) values ('{customer_name}', '{address}', '{postcode}', '{phoneNo}');")
+        self.__execute("SELECT last_insert_id();")
+        return self.cursor.fetchone()[0]
 
-    def create_order(self, cus_id):
-        try:
-            self.execute(
-                f"INSERT INTO order_info(customer) VALUES ({cus_id});")
-            self.execute("SELECT last_insert_id();")
-            return self.cursor.fetchone()[0]
-        except sql.Error as error:
-            print("ERROR: " + str(error))
+    def place_order(self, customer_id, pizzas, side_dishes):
+        self.__execute(f"INSERT INTO order_info(customer) VALUES ({customer_id});")
+        self.__execute("SELECT last_insert_id();")
+        order_id = self.cursor.fetchone()[0]
 
-    def check_coupon(self, coupon_id):
-        if (not self.is_exist('coupon', 'id', coupon_id)):
-            return False
-        else:
-            self.execute(f"SELECT status FROM coupon WHERE id = {coupon_id}")
-            status = self.cursor.fetchone()[0]
-            if (status == 1):
-                return True
-            else:
-                return False
+        for id in pizzas: self.__execute(f"INSERT INTO order_to_pizza(order_info, pizza) VALUES ({order_id}, {id});")
+        for id in side_dishes: self.__execute(f"INSERT INTO order_to_side_dish(order_info, side_dish) VALUES ({order_id}, {id});")
+        self.__execute(f"UPDATE order_info SET time = NOW() WHERE id = {order_id};")
 
-    def send_coupon(self, coupon_id):
-        self.execute(f"UPDATE coupon SET status = 1 WHERE id = {coupon_id};")
-
-    def delete_coupon(self, coupon_id):
-        self.cursor.execute(f"DELETE FROM coupon WHERE id = {coupon_id};")
-
-    def set_order(self, order_id, pizzas, side_dishes):
-        try:
-            for id in pizzas: self.cursor.execute(f"INSERT INTO order_to_pizza(order_info, pizza) VALUES ({order_id}, {id});")
-            for id in side_dishes: self.cursor.execute(f"INSERT INTO order_to_side_dish(order_info, side_dish) VALUES ({order_id}, {id});")
-            self.cursor.execute(f"UPDATE order_info SET time = NOW() WHERE id = {order_id};")
-            
-            self.db.commit() # They should commit together, instead of one by one.
-            return True
-        except sql.Error as error:
-            print("ERROR: " + str(error))
-            return False
+        self.db.commit()
+        return order_id
 
     def delete_order(self, order_id):
-        try:
-            self.cursor.execute(
-                f"DELETE FROM order_to_pizza WHERE order_info = {order_id};")
-            self.cursor.execute(
-                f"DELETE FROM order_to_side_dish WHERE order_info = {order_id};")
-            self.cursor.execute(
-                f"DELETE FROM order_info WHERE id = {order_id};")
-            # They should commit together, instead of one by one.
-            self.db.commit()
-        except sql.Error as error:
-            print("ERROR: " + str(error))
+        self.__execute(f"DELETE FROM order_to_pizza WHERE order_info = {order_id};")
+        self.__execute(f"DELETE FROM order_to_side_dish WHERE order_info = {order_id};")
+        self.__execute(f"DELETE FROM order_info WHERE id = {order_id};")
+        self.db.commit()
+    
+    def check_coupon(self, coupon_id):
+        if (not self.exits('coupon', 'id', coupon_id)): return False
+        self.__execute(f"SELECT status FROM coupon WHERE id = {coupon_id}")
+        status = self.cursor.fetchone()[0]
+        if (status == 1): return True
+        else: return False
+
+    def set_deliveryman_time(self, id):
+        self.__execute(f"UPDATE deliveryman SET time = NOW() WHERE id = {id};")
+    
+    def delete_coupon(self, coupon_id):
+        self.__execute(f"DELETE FROM coupon WHERE id = {coupon_id};")
+
+    def send_coupon(self, coupon_id):
+        self.__execute(f"UPDATE coupon SET status = 1 WHERE id = {coupon_id};")
+    
+    def print_pizza(self, pizza_id):
+        pizza = self.get_pizza(pizza_id)
+        print("  - P" + str(pizza_id) + ": " + pizza["name"].title() + " " + self.is_vegan(pizza["name"]))
+        price = 0
+        for ingredient_name in pizza["ingredients"]:
+            ingredient = self.get_ingredient(ingredient_name)
+            price += ingredient["price"]*1.4
+            print("    - " + ingredient["name"] + " : €" + str("%.2f" % (ingredient["price"]*1.4)))
+        print("    Price: €" + str("%.2f" % (price*1.09)) + " (incl. 9% VAT)\n")
+
+    def print_side_dish(self, side_dish_id):
+        side_dish = self.get_side_dish(side_dish_id)
+        print("  - D" + str(side_dish_id) + ": " + side_dish["name"])
+        print("    Price: €" + str("%.2f" % side_dish["price"]) + " (incl. 9% VAT)\n")
+
+    # "private" function, executes sql command
+    # replaces newlines and tabs with spaces
+    # can run many commands at once, if they are separated with a ";"
+    # if "verbose" is set to true it will print the command before running it, pretty useful for debugging
+    def __execute(self, command):
+        for line in command.replace("\n", " ").replace("\t", " ").split(";"):
+            if (len(line) == 0):  continue
+            try:
+                if verbose: print("RUNNING: " + line)
+                self.cursor.execute(line)
+                self.db.commit()
+            except sql.Error as error: print("ERROR: " + str(error))
 
     # "private" function, check if the "pizza" database exists
     def __sql_database_exists(self):
-        self.execute("SHOW DATABASES LIKE 'pizza';")
+        self.__execute("SHOW DATABASES LIKE 'pizza';")
         return self.cursor.fetchone() != None
 
     # "private" function, creates the "pizza" database
     def __create_sql_database(self):
-        self.execute("CREATE DATABASE pizza;")
-        self.execute("USE pizza;")
-        self.execute(open("database_creation_command.sql", "r").read())
+        self.__execute("CREATE DATABASE pizza;")
+        self.__execute("USE pizza;")
+        self.__execute(open("database_creation_command.sql", "r").read())
 
     # "private" function, destroys the "pizza" database
-    def __destroy_sql_database(self): self.execute("DROP DATABASE pizza;")
+    def __destroy_sql_database(self): self.__execute("DROP DATABASE pizza;")
 
     # "private" function, inserts all the samples from "database_population_command.sql"
-    def __populate_sql_database(self): self.execute(
-        open("database_population_command.sql", "r").read())
-
+    def __populate_sql_database(self): self.__execute(open("database_population_command.sql", "r").read())
 
 # for testing purposes (this will reset the database to the initial state)
 if __name__ == "__main__":
     db = PizzaDatabase()
     db.reset()
-
-    # print(db.is_exist('pizza','id','1'))
-    # print(db.is_exist('pizza','id','11'))
-    #print(db.is_exist(table = 'pizza',col='name',str = 'ham'))
-
-    # print(db.get_cus_info(1))
-    # print(db.get_all_pizzas())
-    # print(db.get_ingredients_for("Vegan Fungi"))
-    # print(db.get_all_pizzas())
-    # print(db.get_pizza_info(1))
-
-    #cus_id = db.create_customer("cat","123","321","345")
-    # print(cus_id)
-    #id = db.create_order(1)
-    # print(id)
-    #items = [[1,2,3],[2]]
-    # print(db.set_order(items,id))
-    # print(type(db.get_total_price(items)))
-    # print(db.get_order_time(1))
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from threading import Thread
 from time import sleep
-# import readline  # importing this lets you use arrow keys and other stuff in input() # Not for Windows
 
 import database
 import datetime
@@ -11,8 +10,8 @@ available commands:
   Prints available pizzas, drinks and desserts
 - order
   Place a new order.
-  format:  order item_1_name item_1_count item_2_name item_2_count ... item_n_name item_n_count
-  example: order margarita 2 orange_juice 2 cheese_cake 1
+- cancel
+  Cancel an existing order
 - reset
   Rest the database to the initial state
 - help
@@ -29,26 +28,25 @@ def input_thread_fn():
     global db, running  # this is a threaded function, so this is needed, idk why
 
     while running:
-        input_str = input("Enter command (\"help\" for available commands)\n > ").strip()
+        input_str = input("Enter command (\"help\" for available commands)\n > ").strip().lower()
 
-        command = input_str.lower().split(" ", 1)[0]
-        args = input_str.lower().split(" ")[1:] if len(input_str.split(" ", 1)) > 1 else []
+        command = input_str.split(" ", 1)[0]
+        args = input_str.split(" ")[1:] if len(input_str.split(" ", 1)) > 1 else []
 
         match command:
-            case "menu":  # TODO: If we can use ID to order that would be more convenient.
+            case "menu": # TODO: If we can use ID to order that would be more convenient.
                 print("- Pizzas:")
-                for pizza_id in db.get_all_pizza_ids(): show_pizza(db, pizza_id)
+                for pizza_id in db.get_all_pizza_ids(): db.print_pizza(pizza_id)
                 print("- Drinks & Deserts:")
-                for side_dish_id in db.get_all_side_dish_ids(): show_side_dish(db, side_dish_id)
+                for side_dish_id in db.get_all_side_dish_ids(): db.print_side_dish(side_dish_id)
 
             case "order":
                 (pizzas, side_dishes) = parse_order(db, args)
                 if pizzas == []: continue
-                customer_id = get_customer(db)
-                order_id = place_order(db, customer_id, pizzas, side_dishes)
+                customer_id = setup_customer(db)
+                order_id = db.place_order(customer_id, pizzas, side_dishes)
 
                 print("Your order id is:", order_id)
-                # coupon(db, items)
                 arrival_time = (db.get_order_time(order_id) + datetime.timedelta(minutes=30)).strftime("%Y-%m-%d %H:%M")
                 print("Your order is expected to arrive at", arrival_time)
 
@@ -67,8 +65,12 @@ def system_thread_fn():
     global db, running  # this is a threaded function, so this is needed, idk why
 
     while running:
+        order_ids = db.get_all_order_ids()
+        for id in order_ids:
+            order = db.get_order(id)
+            if datetime.datetime.now() + datetime.timedelta(minutes=-10) > order["time"]: deliver_order(db, id)
+
         sleep(1)  # only check for updates every second
-        # TODO: implement
 
 # takes in a list of ids and checks if they are all valid
 # if valid, the ids are converted into ids usable directly in the database
@@ -77,8 +79,8 @@ def parse_order(db, ids):
     side_dishes = []
 
     for id in ids: 
-        if id[0] == 'p' and db.is_exist('pizza', 'id', id[1:]): pizzas.append(id[1:])
-        elif id[0] == 'd' and db.is_exist('side_dish', 'id', id[1:]): side_dishes.append(id[1:])
+        if id[0] == 'p' and db.exits('pizza', 'id', id[1:]): pizzas.append(id[1:])
+        elif id[0] == 'd' and db.exits('side_dish', 'id', id[1:]): side_dishes.append(id[1:])
         else:
             print(f"invalid id '{id}'")
             return ([], [])
@@ -89,12 +91,12 @@ def parse_order(db, ids):
 
     print("Ordering:")
 
-    for id in pizzas: show_pizza(db, id)
-    for id in side_dishes: show_side_dish(db, id)
+    for id in pizzas: db.print_pizza(id)
+    for id in side_dishes: db.print_side_dish(id)
 
     return (pizzas, side_dishes)
 
-def get_customer(db):
+def setup_customer(db):
     while(True): # loop until valid customer has been made  
         if input("Do you have an customer ID? (y/n) > ").strip().lower() == 'y':
             id = input("Your customer ID > ").strip()
@@ -111,13 +113,8 @@ def get_customer(db):
         phone = input("Phone number > ").strip()
         return db.create_customer(name, address, postcode, phone)
 
-def place_order(db, customer_id, pizzas, side_dishes):
-    order_id = db.create_order(customer_id)
-    db.set_order(order_id, pizzas, side_dishes)
-    return order_id
-
 def cancel_order(db, id):
-    if db.is_exist("order_info", "id", id):
+    if db.exits("order_info", "id", id):
         if datetime.datetime.now() + datetime.timedelta(minutes=-5) > db.get_order_time(id):
             print(f"Cannot cancel order {id} because for more than 5 minutes have passed.")
         else:
@@ -126,29 +123,14 @@ def cancel_order(db, id):
     else:
         print("Order doesn't exist. Please try again.")
 
-def coupon(db, items):
-    if len(items[0]) >= 10:
-        coupon_id = db.get_coupon()
-        print("You've ordered over 10 pizzas. Here's a coupon id:", coupon_id)
-        print("You can use it for 10% \discount next time.")
-        db.send_coupon(coupon_id)
-
-def show_pizza(db, pizza_id):
-    pizza = db.get_pizza(pizza_id)
-    print("  - P" + str(pizza_id) + ": " +
-          pizza["name"].title() + " " + db.is_vegan(pizza["name"]))
-    price = 0
-    for ingredient_name in pizza["ingredients"]:
-        ingredient = db.get_ingredient(ingredient_name)
-        price += ingredient["price"]*1.4
-        print("    - " + ingredient["name"] + " : €" +
-              str("%.2f" % (ingredient["price"]*1.4)))
-    print("    Price: €" + str("%.2f" % (price*1.09)) + " (incl. 9% VAT)\n")
-
-def show_side_dish(db, side_dish_id):
-    side_dish = db.get_side_dish(side_dish_id)
-    print("  - D" + str(side_dish_id) + ": " + side_dish["name"])
-    print("    Price: €" + str("%.2f" % side_dish["price"]) + " (incl. 9% VAT)\n")
+def deliver_order(db, id):
+    for deliveryman_id in db.get_all_deliverymen_ids():
+        deliveryman = db.get_deliveryman(deliveryman_id)
+        if deliveryman["time"] == None or datetime.datetime.now() + datetime.timedelta(minutes=-20) > deliveryman["time"]:
+            print(f"{deliveryman['name']} is now delivering order {id}, it will arrive in 20 minutes")
+            db.delete_order(id)
+            db.set_deliveryman_time(id)
+            return
 
 def check_coupon(db):
     while (True):
@@ -160,6 +142,13 @@ def check_coupon(db):
             return 0.9
         else:
             print("Invalid coupon. Please try again.")
+
+def coupon(db, items):
+    if len(items[0]) >= 10:
+        coupon_id = db.get_coupon()
+        print("You've ordered over 10 pizzas. Here's a coupon id:", coupon_id)
+        print("You can use it for 10% \discount next time.")
+        db.send_coupon(coupon_id)
 
 if __name__ == "__main__":
     print("Starting app")
